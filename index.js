45ea6242ad269bfb93ed6568badc131d92b2aec1
Dhanush <99753690+Dbdhanush@users.noreply.github.com>
2023-03-21 11:11:22 +0530

    dark mode added successfully

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const toggler = document.getElementById('sun');
 const body = document.querySelector('body');
 
 toggler.addEventListener('click', function(){
-this.classList.toggler('fa-moon');
-if(this.classList.toggler('fa-sun'))
+this.classList.toggle('fa-moon');
+if(this.classList.toggle('fa-sun'))
 {
 body.style.backgroundColor = 'white';
 body.style.color = 'black';
